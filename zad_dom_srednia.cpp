12489c5ec34e8360088e80e24584a701d9b1a4c4
RadoslawP <radoslaw.pietraszek.r@gmail.com>
2020-06-25 07:06:39 +0200

    zadanie domowe srednia

---
+++ zad_dom_srednia.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+float a, b, c, d, e, suma, srednia;
+float sa, sb, sc, sd, se;
+int main()
+{
+    cout <<"Podaj prosze 5 liczb: "<< endl;
+    cin>>a>>b>>c>>d>>e;
+
+    suma=a+b+c+d+e;
+    srednia=suma/5;
+    sa=
+    sb
+    sc
+    sd
+    se
+    cout<<"suma to "<<suma<<endl;
+    cout<<"srednia to "<<srednia;
+    cout<<;
+
+
+    return 0;
+}
